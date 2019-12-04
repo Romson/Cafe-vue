68529555ef41f2cb620fa72d43928fc2d3e7f8c8
Romero Lijkwan <romero@uniware.nl>
2019-12-04 14:02:16 +0100

    Routes

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <App />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import App from '../App.vue';
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+    App
+  }
+}
+</script>
